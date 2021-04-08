c6ef0940c0a24aaa87b67ddb056613d84a850cc9
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-08 11:44:16 +0200

    added a signup and login route and added the imports in index file accordigly

--- index.js
+++ index.js
@@ -2,8 +2,40 @@ const express = require("express");
 const loggerMiddleWare = require("morgan");
 const corsMiddleWare = require("cors");
 const { PORT } = require("./config/constants");
+const authRouter = require("./routers/auth");
+const authMiddleWare = require("./auth/middleware");
 
 const app = express();
+
+app.use(loggerMiddleWare("dev"));
+
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+app.use(corsMiddleWare());
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
+app.post("/authorized_post_request", authMiddleWare, (req, res) => {
+  const user = req.user;
+
+  delete user.dataValues["password"];
+
+  res.json({
+    youPosted: {
+      ...req.body,
+    },
+    userFoundWithToken: {
+      ...user.dataValues,
+    },
+  });
+});
+
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
